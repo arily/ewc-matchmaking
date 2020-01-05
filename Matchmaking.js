26810b486b5bdc86d1c55c0165373f85fc9d2c33
arily <lbsaaaaa@gmail.com>
2020-01-05 22:23:11 +0900

    Initial commit

---
+++ Matchmaking.js
@@ -0,0 +1,38 @@
+const EApi = require("EApi");
+const E = new EApi();
+
+function Matchmaking(options = {}) {
+    this.options = options;
+    this.players = [];
+    this.timeouts = {};
+}
+
+Matchmaking.prototype.putIn = function(player) {
+    this.players.push(player);
+    this.timeouts[player.id] = setTimeout((player)=>this.remove(player), this.options.timeout || 1000 * 60 * 60 * 6, player);
+    return true;
+}
+Matchmaking.prototype.remove = function(player) {
+    this.players = this.players.filter(match => match.qq !== player.qq);
+
+    if (this.timeouts[player.id] !== undefined) {
+        clearTimeout(this.timeouts[player.id]);
+    }
+
+}
+Matchmaking.prototype.findByQQ = function(qq) {
+    return this.players.filter(match => match.qq == qq);
+}
+Matchmaking.prototype.list = function() {
+    return JSON.parse(JSON.stringify(this.players));
+}
+Matchmaking.prototype.findPlayerInRange = function(player, range = 400) {
+    range = {
+        max: player.elo + range,
+        min: player.elo - range
+    }
+    let suitablePlayer = this.players.filter(match => range.max >= match.elo && match.elo >= range.min && match.id !== player.id);
+    return suitablePlayer;
+}
+
+module.exports = Matchmaking;
---
+++ Player.js
@@ -0,0 +1,32 @@
+const AbortController = require('abort-controller');
+const EApi = require("EApi");
+const E = new EApi();
+
+function timeoutSignal(timeout = 5) {
+    const controller = new AbortController()
+    const signal = controller.signal
+    setTimeout(() => {
+        controller.abort()
+    }, timeout * 1000)
+    return signal;
+}
+
+function MatchmakingPlayer() {
+
+}
+MatchmakingPlayer.prototype.init = async function(player) {
+    this.qq = player.qq;
+    this.id = player.id;
+    this.name = player.name;
+    let elo = await E.users.elo(this.id, { signal: timeoutSignal(20) })
+    this.fob = (elo.code == 40004);
+    this.elo = elo.elo;
+    this.createdAt = new Date().getTime();
+}
+create = async function(player) {
+    const obj = new MatchmakingPlayer();
+    await obj.init(player);
+    return obj;
+}
+
+module.exports = create;
